9ba39aac1ebe14aba852286dad8528290d55c673
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2018-04-23 15:11:24 +0000

    patch export_inference_graph.py for exporting with tf 1.3

--- research/object_detection/export_inference_graph.py
+++ research/object_detection/export_inference_graph.py
@@ -93,9 +93,9 @@ flags.DEFINE_string('trained_checkpoint_prefix', None,
                     'path/to/model.ckpt')
 flags.DEFINE_string('output_directory', None, 'Path to write outputs.')
 
-tf.app.flags.mark_flag_as_required('pipeline_config_path')
-tf.app.flags.mark_flag_as_required('trained_checkpoint_prefix')
-tf.app.flags.mark_flag_as_required('output_directory')
+# tf.app.flags.mark_flag_as_required('pipeline_config_path')
+# tf.app.flags.mark_flag_as_required('trained_checkpoint_prefix')
+# tf.app.flags.mark_flag_as_required('output_directory')
 FLAGS = flags.FLAGS
 
 
